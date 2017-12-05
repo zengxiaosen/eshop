49c101d15a51d3fe50584ec68bcabd39ff6389b1
zengxiaosen <919451814@qq.com>
2017-12-05 16:11:44 +0800

    test

--- src/main/java/com/z/kafkaconsumer/StringUtil.java
+++ src/main/java/com/z/kafkaconsumer/StringUtil.java
@@ -3,6 +3,41 @@ package com.z.kafkaconsumer;
 /**
  * Created by root on 17-12-5.
  */
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+/**
+ * 字符串工具类
+ */
 public class StringUtil {
+    /**
+     * |是正则表示特殊字符
+     */
+    private static final String token = "\\|\\|\\|" ;
+    /**
+     * 切割单行日志
+     */
+    public static String[] splitLog(String log){
+        String[] arr = log.split(token);
+        return arr;
+    }
 
+    public static String getHostname(String[] arr){
+        return arr[0];
+    }
+
+    /**
+     *返回 2017/02/28/12/12
+     */
+    public static String formatYyyyMmDdHhMi(String[] arr){
+        try{
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MMM/yyyy:HH:mm:ss", Locale.US);
+            Date d = sdf.parse(arr[3].split(" ")[0]);
+            SimpleDateFormat localSDF = new SimpleDateFormat("yyyy/MM/dd/HH/mm", Locale.US);
+            return localSDF.format(d);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
